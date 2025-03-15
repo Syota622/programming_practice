f73fc827667b296d5089fab976764c38d95b9971
Shota <wvp360gqanukcqhg@gmail.com>
2025-03-15 22:26:04 +0900

    辞書型

---
+++ typescript/src/index.js
@@ -0,0 +1,5 @@
+var nicname = "tarou";
+var age = 20;
+var isActivate = true;
+var fruits = ["りんご", "みかん", "パイナップル"];
+fruits.forEach(function (fruit) { return fruit.length; });
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,6 @@
 let nicname: string = "tarou";
 let age: number = 20;
 let isActivate: boolean = true;
+let fruits: string[] = ["りんご", "みかん", "パイナップル"];
+
+fruits.forEach((fruit) => fruit.length);
